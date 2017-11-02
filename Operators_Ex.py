34a9a1e42f5c993ea40e10a3c97a8237c5f2dd09
allamrajukrish <srikrishna@stellentsoft.com>
2017-11-02 20:17:56 +0530

    First Commit

---
+++ Operators_Ex.py
@@ -0,0 +1,45 @@
+#Basic arithemetic operators
+#addition
+a = 12
+b = 13
+c = a+b
+print (c)
+
+x = 7.9
+y = 3
+z = x+y
+print(z)
+
+'''Print the value along with text, 
+Here we need to use format specifier to 
+print the value along with text. In the below example 
+I'm trying to print a float value so 
+I used %f as a format specifier '''
+print("The value of z is %f" %z)
+
+#Subtraction
+val1 = 32
+val2 = 20
+val3 = val1-val2
+print(val3)
+
+var1 = 10
+var2 = 7.7
+var3 = var2-var1
+print(var3)
+
+#Multiplication
+print("Final value of Multiplication is %d" %(val1 * val2))
+print(var1*var2)
+
+#Division
+divResult = 2/3
+print(divResult)
+
+modResult = 17%3
+print(modResult)
+
+
+
+
+
